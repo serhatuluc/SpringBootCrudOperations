02d71e2b536c58865d7d3691686d28349d29c780
serhatuluc <ulucs15@itu.edu.tr>
2022-12-13 01:12:40 +0300

    Update service is added

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,12 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-import java.time.Month;
+
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Component
@@ -35,5 +36,23 @@ public class StudentService {
             throw new IllegalStateException("student with id " + studentId + " doesnt exist");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(()->new IllegalStateException("student with id " + studentId + " doesnt exist"));
+
+        if(name != null && name.length() > 0 && !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
+
+        if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(),email)){
+            Optional<Student> studentOptional = studentRepository.findStudentsByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
